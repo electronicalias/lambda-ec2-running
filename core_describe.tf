22807096daed9622019ccb70e96b4d736e5d0df5
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 22:18:18 +0100

    change to the lambdas.zip

--- core_describe.tf
+++ core_describe.tf
@@ -18,9 +18,9 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-    filename = "lambda.zip"
+    filename = "lambdas.zip"
     function_name = "core_describe"
     role = "${aws_iam_role.iam_for_lambda.arn}"
     handler = "lambda_handler"
-    source_code_hash = "${base64sha256(file("lambda.zip"))}"
+    source_code_hash = "${base64sha256(file("lambdas.zip"))}"
 }
