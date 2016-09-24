2c01a51c397515984d166bf83c2ac712d3faaf32
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 22:39:32 +0100

    reformat the IAM policy

--- core_describe.tf
+++ core_describe.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "iam_for_lambda" {
     name = "iam_for_lambda"
     assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
-    EOF
+  ]
+}
+EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
