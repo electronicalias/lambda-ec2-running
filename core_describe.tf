a0239e03dda36fe1695ee75a16b635767d55ee08
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 23:32:50 +0100

    push a policy document

--- core_describe.tf
+++ core_describe.tf
@@ -1,3 +1,22 @@
+resource "aws_iam_role_policy" "test_policy" {
+    name = "test_policy"
+    role = "${aws_iam_role.iam_for_lambda.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
     name = "iam_for_lambda"
     assume_role_policy = <<EOF
