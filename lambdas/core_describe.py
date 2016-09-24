f8a6793d997e184e937bd8454e86eecb22eec501
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 23:46:25 +0100

    adding some instance code  to print something meaning full out

--- lambdas/core_describe.py
+++ lambdas/core_describe.py
@@ -4,3 +4,6 @@ rname = 'eu-west-1'
 def lambda_handler(event, context):
 	ec2 = boto3.client('ec2', rname)
 	print(ec2.describe_instances())
+	for item in data['Reservations']:
+	    for instance in item['Instances']:
+	        print instance['InstanceId']
