0d1efcf43e2f5c1cf855d05cb27c2b0ba5fca197
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 22:49:48 +0100

    adding runtime

--- core_describe.tf
+++ core_describe.tf
@@ -23,4 +23,5 @@ resource "aws_lambda_function" "test_lambda" {
     role = "${aws_iam_role.iam_for_lambda.arn}"
     handler = "lambda_handler"
     source_code_hash = "${base64sha256(file("lambdas.zip"))}"
+    runtime = "python2.7"
 }
