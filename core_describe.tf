841509f23f3ed316d766996542a6c2cdf3ca6408
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 23:06:07 +0100

    correction the name of the handler

--- core_describe.tf
+++ core_describe.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "test_lambda" {
     filename = "lambdas.zip"
     function_name = "core_describe"
     role = "${aws_iam_role.iam_for_lambda.arn}"
-    handler = "lambda_handler"
+    handler = "core_describe.lambda_handler"
     source_code_hash = "${base64sha256(file("lambdas.zip"))}"
     runtime = "python2.7"
 }
