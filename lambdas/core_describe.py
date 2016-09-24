b8856f819eed2ab5b1a2eab95bba47bbbd83bbd0
Philip Smith <philip.smith@nordcloud.com>
2016-09-24 23:50:59 +0100

    missed the data variable that was referred to later

--- lambdas/core_describe.py
+++ lambdas/core_describe.py
@@ -3,7 +3,7 @@ rname = 'eu-west-1'
 
 def lambda_handler(event, context):
 	ec2 = boto3.client('ec2', rname)
-	print(ec2.describe_instances())
+	data = ec2.describe_instances()
 	for item in data['Reservations']:
 	    for instance in item['Instances']:
 	        print instance['InstanceId']
