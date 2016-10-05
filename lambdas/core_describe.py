3e08628e9028e711abcfd15568998dace2ecf158
Philip Smith <philip.smith@nordcloud.com>
2016-10-05 10:34:31 +0300

    creating new try state for the lambda function

--- lambdas/core_describe.py
+++ lambdas/core_describe.py
@@ -5,16 +5,26 @@ def lambda_handler(event, context):
 	ec2 = boto3.client('ec2', rname)
 	instance_ids = {}
 	count = 0
-	state_query = event['params']['querystring']['state']
-	body_json = ec2.describe_instances(
-	    Filters=[
-        {
-            'Name': 'instance-state-name',
-            'Values': [
-                state_query,
-            ]
-        },
-    ])
+
+    def fetch_instances():
+        try:
+        	if event['params']['querystring']['state'] is None:
+        		full_json = ec2.describe_instances()
+    		    return full_json
+        	else:
+        		state_query = event['params']['querystring']['state']
+        		full_json = ec2.describe_instances(
+	    			Filters=[
+        				{
+            				'Name': 'instance-state-name',
+            				'Values': [
+                				state_query,
+            				]
+        				},
+    			])
+    		    return full_json
+
+	body_json = fetch_instances()
 	for item in body_json['Reservations']:
 	    for instance in item['Instances']:
 	        instance_ids['instance_id' + "_" + str(count)] = instance['InstanceId']
