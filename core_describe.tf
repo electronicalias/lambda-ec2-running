d73d795193aa30804188b629014d450c2cca6b54
Philip Smith <philip.smith@nordcloud.com>
2016-10-05 12:03:18 +0300

    changing the lambda function for two environments

--- core_describe.tf
+++ core_describe.tf
@@ -79,7 +79,7 @@ EOF
 
 resource "aws_lambda_function" "getInstancesLambda" {
     filename = "lambdas.zip"
-    function_name = "core_describe"
+    function_name = "core_describe_${var.build_stage}"
     role = "${aws_iam_role.terra_iam_for_lambda.arn}"
     handler = "core_describe.lambda_handler"
     source_code_hash = "${base64sha256(file("lambdas.zip"))}"
